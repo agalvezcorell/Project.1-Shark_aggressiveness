1ae22de94c95d5789db407b517a5477e1f3a2e99
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-01-26 18:45:19 +0100

    Categorizadas las lesiones en gravedad 1,2,3

--- src/funciones.py
+++ src/funciones.py
@@ -8,3 +8,40 @@ def limpiadate(x):
     return x
 
 
+def assignscore3(x):
+    if x == "Y":
+        x = 3
+    else:
+        x = "N"
+    return x
+
+
+def assingscore(x):
+    nivel2 = "2"
+    nivel1 = "1"
+    nivel3 = "3"
+    if type(x) != str:
+        x = nivel2
+    if re.search (r"(.*)?acera(.*)?", x) != None:
+        x = nivel1
+    if re.search (r"(.*)?o inju(.*)?", x) != None:
+        x = nivel1
+    if re.search (r"(.*)?inor(.*)?", x) != None:
+        x = nivel1     
+    if re.search (r"(.*)?o in(.*)?", x) != None:
+        x = nivel1
+    if re.search (r"(.*)?FATAL(.*)?", x) !=None:
+        x= nivel3
+    if re.search (r"(.*)? (.*)?", x) != None:
+        x= nivel2
+    if re.search (r"(.*)?urvi(.*)?", x) != None:
+        x= nivel2
+    if re.search (r"(.*)?on(.*)?", x) != None:
+        x= nivel2
+    if re.search (r"(.*)?bras(.*)?", x) != None:
+        x= nivel2
+    if re.search (r"(.*)?over(.*)?", x) != None:
+        x= nivel2
+    if re.search (r"(.*)?aule(.*)?", x) != None:
+        x= nivel2
+    return x
