21230435374c5e9f35ce28121f41e515a1c2e0bd
Amanda Gálvez Corell <agalvezcorell@gmail.com>
2020-01-27 21:27:41 +0100

    Terminado, he disfrutado mucho este proyecto 🦈

--- src/funciones.py
+++ src/funciones.py
@@ -16,35 +16,34 @@ def asigna3(x):
     return x
 
 
-def asignapuntos(x):
-    nivel2 = "2"
+
+level1 = ["(.*)?acera(.*)?", "(.*)?o inju(.*)?","(.*)?inor(.*)?","(.*)?o in(.*)?","(.*)?ruised(.*)?" ] 
+level2 = ["(.*)?urvi(.*)?", "(.*)?on(.*)?", "(.*)?bras(.*)?","(.*)? (.*)?","(.*)?over(.*)?","(.*)?aule(.*)?"]
+level3 = ["(.*)?FATAL(.*)?"]
+
+
+def assingscore(x):
+    import re
     nivel1 = "1"
+    nivel2 = "2"
     nivel3 = "3"
     if type(x) != str:
         x = nivel2
-    if re.search (r"(.*)?acera(.*)?", x) != None:
-        x = nivel1
-    if re.search (r"(.*)?o inju(.*)?", x) != None:
-        x = nivel1
-    if re.search (r"(.*)?inor(.*)?", x) != None:
-        x = nivel1     
-    if re.search (r"(.*)?o in(.*)?", x) != None:
-        x = nivel1
-    if re.search (r"(.*)?FATAL(.*)?", x) !=None:
-        x= nivel3
-    if re.search (r"(.*)? (.*)?", x) != None:
-        x= nivel2
-    if re.search (r"(.*)?urvi(.*)?", x) != None:
-        x= nivel2
-    if re.search (r"(.*)?on(.*)?", x) != None:
-        x= nivel2
-    if re.search (r"(.*)?bras(.*)?", x) != None:
-        x= nivel2
-    if re.search (r"(.*)?over(.*)?", x) != None:
-        x= nivel2
-    if re.search (r"(.*)?aule(.*)?", x) != None:
-        x= nivel2
-    return x
+        return x
+    if x == None:
+        x = nivel2
+    for a in level1:
+        if re.search (a,x) != None:
+            x = nivel1
+            return x
+    for b in level2:
+        if re.search (b,x) != None:
+            x = nivel2
+            return x
+    for c in level3:
+        if re.search (c,x) != None:
+            x = nivel3
+            return x
 
 
 def casteascore(x):
